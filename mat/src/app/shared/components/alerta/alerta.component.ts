932bda2d5d3595b055da0f766923e85db66a0200
Marcilio <marciliorsilva93@gmail.com>
2020-12-09 11:15:15 -0300

    passando configurações para o alerta(modal)

--- mat/src/app/shared/components/alerta/alerta.component.ts
+++ mat/src/app/shared/components/alerta/alerta.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Alerta } from '../../models/alerta';
 
 @Component({
   selector: 'dio-alerta',
@@ -8,17 +9,31 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class AlertaComponent implements OnInit {
 
-  titulo = 'Sucesso';
-  descricao = 'Seu registro foi cadastrado com sucesso!';
-  btnSucesso = 'Ok';
-  btnCancel = "Cancelar";
-  corBtn = "primary";
-  possuirBtnFechar = false;
+  alerta = {
+    titulo:'Sucesso',
+    descricao:'Seu registro foi cadastrado com sucesso!',
+    btnSucesso:'Ok',
+    btnCancelar:"Cancelar",
+    corBtnSucesso:"accent",
+    corBtnCancelar:"warn",
+    possuirBtnFechar: false
+  } as Alerta;
+ 
 
   constructor(public dialogRef: MatDialogRef<AlertaComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {}
+    @Inject(MAT_DIALOG_DATA) public data: Alerta) {}
 
   ngOnInit() {
+    if(this.data){
+      this.alerta.titulo = this.data.titulo || this.alerta.titulo;
+      this.alerta.descricao = this.data.descricao || this.alerta.descricao;
+      this.alerta.btnSucesso = this.data.btnSucesso || this.alerta.btnSucesso;
+      this.alerta.btnCancelar = this.data.btnCancelar || this.alerta.btnCancelar;
+      this.alerta.corBtnSucesso = this.data.corBtnSucesso || this.alerta.corBtnSucesso;
+      this.alerta.corBtnCancelar = this.data.corBtnCancelar || this.alerta.corBtnCancelar;
+      this.alerta.possuirBtnFechar = this.data.possuirBtnFechar || this.alerta.possuirBtnFechar;
+      
+    }
   }
 
 }
